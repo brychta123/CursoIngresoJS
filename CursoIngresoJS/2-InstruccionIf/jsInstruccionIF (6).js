f3057fdb350683f2fdd52d64da16b8008530cb1c
brychta123 <47573725+brychta123@users.noreply.github.com>
2019-02-20 21:43:23 -0300

    20/2

--- CursoIngresoJS/2-InstruccionIf/jsInstruccionIF (6).js
+++ CursoIngresoJS/2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,25 +1,7 @@
 function mostrar()
 {
-
 //tomo la edad  
-    var edad;
-    edad=document.getElementById("edad").value;
 
-    if(edad>=18) {
-
-        alert ("sos mayor de edad");
-    }
-    else
-    {
-        
-        if(edad>=13) {
-            alert("usted es adolescente");
-            
-        } else {
-            alert("sos menor");
-        }
-    }
-  
 
 
 
--- CursoIngresoJS/2-InstruccionIf/jsInstruccionIF (7).js
+++ CursoIngresoJS/2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,18 +1,8 @@
 function mostrar()
 {
 //tomo la edad  
-  var edad;
 
-  var estadocivil;
-
-  edad=document.getElementById("edad");
-
-  estadocivil=document.getElementById("estadoCivil");
-
-  {if(edad<18)
-     alert("es muy pequeño para no ser soltero");
-}
 	
 
 
-//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN
